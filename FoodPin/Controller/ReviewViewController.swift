6dba65718262794a5a160792c46e6769691ec481
LienPaul <zinfu1111@gmail.com>
2020-08-09 20:55:04 +0800

    完成第17張作業

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -12,6 +12,7 @@ class ReviewViewController: UIViewController {
 
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var rateButtons: [UIButton]!
+    @IBOutlet var closeButton: UIButton!
     
     var restaurant = Restaurant()
     
@@ -39,12 +40,15 @@ class ReviewViewController: UIViewController {
             rateButton.alpha = 0
         }
         
+        let moveTopTransform = CGAffineTransform.init(translationX: 0, y: -600)
+        closeButton.transform = moveTopTransform
+        closeButton.alpha = 0
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         
-        //1.全部動畫
+        //1.全部動畫淡出
 //        UIView.animate(withDuration: 2.0, animations: {
 //            for rateButton in self.rateButtons {
 //                rateButton.alpha = 1.0
@@ -60,29 +64,16 @@ class ReviewViewController: UIViewController {
 //        }, completion: nil)
         
         //2-2滑入動畫示範
+        for i in 0...4 {
+            UIView.animate(withDuration: 0.4, delay: 0.1+(Double(i)*0.05), options: [], animations: {
+                self.rateButtons[i].alpha = 1.0
+                self.rateButtons[i].transform = .identity
+            }, completion: nil)
+        }
+        
         UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
-            self.rateButtons[0].alpha = 1.0
-            self.rateButtons[0].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
-            self.rateButtons[1].alpha = 1.0
-            self.rateButtons[1].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
-            self.rateButtons[2].alpha = 1.0
-            self.rateButtons[2].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
-            self.rateButtons[3].alpha = 1.0
-            self.rateButtons[3].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
-            self.rateButtons[4].alpha = 1.0
-            self.rateButtons[4].transform = .identity
+            self.closeButton.alpha = 1.0
+            self.closeButton.transform = .identity
         }, completion: nil)
         
     }
